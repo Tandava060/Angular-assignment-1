762527c81ae4ba79056e5b18e24f259aea50af0a
Tandava <appadooashwin@gmail.com>
2021-06-02 13:58:32 +0400

    Angular part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish {
+    id!: string;
+    name!: string;
+    image!: String;
+    category!: string;
+    featured!: boolean;
+    label!: string;
+    price!: string;
+    description!: string;
+}
